7e9264439d700fe9d0298b9c453c4ac504f4e0d2
Mansi Bisaria <mansibisaria@cedcoss.com>
2023-03-28 17:24:51 +0530

    Commit

--- src/index.php
+++ src/index.php
@@ -11,7 +11,7 @@ $bill = 0;
 if ($units <= 0 && isset($units)) {
     echo "Enter valid units";
 } else if ($units <= 50) {
-    $bill = ($units - 50) * 3.50;
+    $bill = $units * 3.50;
 } elseif ($units > 50 && $units <= 150) {
     $bill = 50 * 3.50 + ($units - 50) * 4;
 } elseif ($units > 150 && $units <= 250) {
